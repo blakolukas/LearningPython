fa05fd9c76725131940a6204eebb21779ad65daa
blakolukas <lucasblacovicz@gmail.com>
2024-04-25 19:52:45 -0300

    trab 6

---
+++ trab 6/Duplas.py
@@ -0,0 +1,25 @@
+from random import randint
+
+nomes= []
+duplas=[["",""],["",""]]
+
+while len(nomes)<5:
+    bota= input("\nnome:")
+    nomes.append(bota)
+
+print(f"nomes: {nomes}")
+a=0
+while(True): 
+    b=0
+    c= randint(0,len(nomes)-1)
+    duplas[a][b]= nomes[c]
+    nomes.remove(nomes[c])
+    b=1
+    c= randint(0,len(nomes)-1)
+    duplas[a][b]= nomes[c]
+    nomes.remove(nomes[c])
+    a+=1
+    if len(nomes)==0:
+        break
+
+print(f"\nduplas: {duplas}")
