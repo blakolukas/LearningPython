b1e2f6a928eea70012ad7598e844ec4337e4c611
blakolukas <lucasblacovicz@gmail.com>
2024-06-20 20:30:19 -0300

    att

--- aula 9/Mass.py
+++ aula 9/Mass.py
@@ -23,13 +23,18 @@ graph.show()
 v= int(input("gerar gráfico?\n1-sim\n2-não\n"))
 if v==1:
     listP=[]
+    listL=[]
     t=0
-    while t<len(listPeso)-1:
-        p= 1-(listPeso[t+1]/listPeso[t])*100
+
+    while t<(len(listPeso)-1):
+        p= (1-(listPeso[t+1]/listPeso[t]))*100
+        first= (1-(listPeso[t+1]/listPeso[0]))*100
         listP.append(p)
+        listL.append(first)
         t+=1
+   
     graph.figure(2)
-    graph.plot(arange(1,i),listP,'o--')
+    graph.plot(arange(1,i),listP,'o--',arange(1,i),listL,'ro--')
     graph.title("peso")
     graph.xlabel("tempo")
     graph.ylabel("%")
